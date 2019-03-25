2976994226db6d6c13a8abc6afc6d47c70c64846
mealvarado <mealvarado@uc.cl>
2019-03-25 11:58:53 -0300

    deploy

--- myapp/views.py
+++ myapp/views.py
@@ -56,19 +56,99 @@ def character(response, character):
     url = 'https://swapi.co/api/people/' + str(character)
     response = requests.get(url)
     data = response.json()
-    return render_to_response('character.html', {"character" : data})
+    character = {}
+    for k,v in data.items():
+        if (k == "edited" or k == "created" or k == "url"):
+            pass
+        elif k == "starships":
+            names = {}
+            for n in data["starships"]:
+                response_starships = requests.get(n)
+                data_starships = response_starships.json()
+                names[data_starships["name"]] = n.split('/')[-2]
+            character[k] = names
+        elif k == "vehicles":
+            names = {}
+            for n in data["vehicles"]:
+                response_vehicles = requests.get(n)
+                data_vehicles = response_vehicles.json()
+                names[data_vehicles["name"]] = n.split('/')[-2]
+            character[k] = names
+        elif k == "species":
+            names = {}
+            for n in data["species"]:
+                response_species = requests.get(n)
+                data_species = response_species.json()
+                names[data_species["name"]] = n.split('/')[-2]
+            character[k] = names
+        elif k == "homeworld":
+            names = {}
+            response_homeworld = requests.get(v)
+            data_homeworld = response_homeworld.json()
+            names[data_homeworld["name"]] = v.split('/')[-2]
+            character[k] = names
+        elif k == "films":
+            names = {}
+            for n in data["films"]:
+                response_film = requests.get(n)
+                data_film = response_film.json()
+                names[data_film["title"]] = n.split('/')[-2]
+            character[k] = names
+        else:
+            character[k] = v
+    return render_to_response('character.html', {"character" : character})
 
 def planet(response, planet):
     url = 'https://swapi.co/api/planets/' + str(planet)
     response = requests.get(url)
     data = response.json()
-    return render_to_response('planet.html', {"planet" : data})
+    planet = {}
+    for k,v in data.items():
+        if (k == "edited" or k == "created" or k == "url"):
+            pass
+        elif k == "residents":
+            names = {}
+            for n in data["residents"]:
+                response_residents = requests.get(n)
+                data_residents = response_residents.json()
+                names[data_residents["name"]] = n.split('/')[-2]
+            planet[k] = names
+        elif k == "films":
+            names = {}
+            for n in data["films"]:
+                response_film = requests.get(n)
+                data_film = response_film.json()
+                names[data_film["title"]] = n.split('/')[-2]
+            planet[k] = names
+        else:
+            planet[k] = v
+    return render_to_response('planet.html', {"planet" : planet})
 
 def starship(response, starship):
     url = 'https://swapi.co/api/starships/' + str(starship)
     response = requests.get(url)
     data = response.json()
-    return render_to_response('starship.html', {"starship" : data})
+    starship = {}
+    for k,v in data.items():
+        if (k == "edited" or k == "created" or k == "url"):
+            pass
+        elif k == "pilots":
+            names = {}
+            for n in data["pilots"]:
+                response_pilots = requests.get(n)
+                data_pilots = response_pilots.json()
+                names[data_pilots["name"]] = n.split('/')[-2]
+            starship[k] = names
+        elif k == "films":
+            names = {}
+            for n in data["films"]:
+                response_film = requests.get(n)
+                data_film = response_film.json()
+                names[data_film["title"]] = n.split('/')[-2]
+            starship[k] = names
+        else:
+            starship[k] = v
+    return render_to_response('starship.html', {"starship" : starship})
 
 
 @csrf_exempt
@@ -96,7 +176,7 @@ def result(request):
         for i in starships["results"]:
             starships_res[i["name"]] = i["url"].split('/')[-2]
 
-    url_planets = 'https://swapi.co/api/films/?search=' + str(search_query)
+    url_planets = 'https://swapi.co/api/planets/?search=' + str(search_query)
     response_planets = requests.get(url_planets)
     planets = response_planets.json()
     planets_res = {}
@@ -106,7 +186,7 @@ def result(request):
         for i in planets["results"]:
             planets_res[i["name"]] = i["url"].split('/')[-2]
 
-    url_films = 'https://swapi.co/api/planets/?search=' + str(search_query)
+    url_films = 'https://swapi.co/api/films/?search=' + str(search_query)
     response_films = requests.get(url_films)
     films = response_films.json()
     films_res = {}
@@ -114,7 +194,7 @@ def result(request):
         pass
     else:
         for i in films["results"]:
-            films_res[i["name"]] = i["url"].split('/')[-2]
+            films_res[i["title"]] = i["url"].split('/')[-2]
 
-    query = {"people_res":people_res,"starships":starships_res,"planets":planets_res,"films":films_res}
+    query = {"people_res":people_res,"starships_res":starships_res,"planets_res":planets_res,"films_res":films_res}
     return render_to_response('result.html',{'query':query})
